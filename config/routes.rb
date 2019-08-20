b9024fab544cd120fe954ff6067eef66d5444f8f
axelle bourneuf <axellebourneuf@hotmail.fr>
2019-08-20 18:20:21 +0200

    dashboard update

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
    get "dashboard", to: "users#dashboard"
-  resources :kitchens do
-     
+  resources :kitchens do   
     resources :bookings do
     end
   end
