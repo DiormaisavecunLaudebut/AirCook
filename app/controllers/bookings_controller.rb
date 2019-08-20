9e48f1d2b0acc0fb62a97b68698d01e88eb4bcc5
axelle bourneuf <axellebourneuf@hotmail.fr>
2019-08-20 15:08:14 +0200

    booking action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,29 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
   def new
+    @booking = Booking.new
   end
 
   def create
-  end
-
-  def index
+    booking = booking.new(booking_params)
+    booking.user = current_user
+    booking.save ? (redirect_to  bookings_path) : (render 'new')
   end
 
   def destroy
+  	@booking = Booking.find(params[:id]
+    booking.user = current_user
+    @booking.destroy
+    redirect_to bookings_path
+
+  end
+
+  private 
+
+  def booking_params
+  	params.require(:booking).permit(:start_date, :end_date)
   end
 end
