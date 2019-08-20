4775e42d536f673f24f17619eeccdc62bbb56434
axelle bourneuf <axellebourneuf@hotmail.fr>
2019-08-20 18:24:43 +0200

    update dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'lior', to: 'pages#lior'
    get "dashboard", to: "users#dashboard"
-<<<<<<< HEAD
-  resources :kitchens do   
-=======
   resources :kitchens do
-
->>>>>>> 4936f7f1409d7b38222d8741a2cf36fcc7017d39
     resources :bookings do
     end
   end
