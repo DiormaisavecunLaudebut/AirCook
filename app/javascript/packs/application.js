3ed700779a73fb695e5a13cf41172aff61a18fb1
Lior Levy <lior.pmtraining@gmail.com>
2019-08-21 15:26:22 +0200

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,9 @@
 import "bootstrap";
-import { like, dislike } from '../components/card-like';
+import { like } from '../components/card-like';
 import "../plugins/flatpickr"
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
-like();
-dislike();
 initMapbox();
+like();
 
