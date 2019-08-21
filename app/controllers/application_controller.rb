89a84f8377ff6b4b49199fe3cd400c758a7a6deb
Lior Levy <lior.pmtraining@gmail.com>
2019-08-21 16:10:03 +0200

    fixed username bug and added username to navbar while signed up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   after_action :verify_authorized, except: [:index, :mykitchens], unless: :skip_pundit?
@@ -15,6 +16,12 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :photo)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :photo)}
   end
 end
 
