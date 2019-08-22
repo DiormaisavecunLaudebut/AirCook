60f8ce0ee5e9cadd16347f3813607c0b7517bb18
Marion daudin <mariondaudin@icloud.com>
2019-08-22 18:54:31 +0200

    kk

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,10 @@ import { like } from '../components/card-like';
 import "../plugins/flatpickr"
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
+import { previewImageOnFileSelect } from '../components/photo-preview';
+
 
 initMapbox();
 like();
+previewImageOnFileSelect();
 
