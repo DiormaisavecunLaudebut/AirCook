1a7ff544c7ee68bac1ac7af6636a98c00ccd6c30
Lior Levy <lior.pmtraining@gmail.com>
2019-08-23 14:19:57 +0200

    heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,6 @@ import { previewImageOnFileSelect } from '../components/photo-preview';
 import { calculateBooking } from '../components/calculate-booking-price';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
 
 previewImageOnFileSelect();
 initMapbox();
