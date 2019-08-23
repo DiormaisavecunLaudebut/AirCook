625083e862b7c0fc61ab34808a4733ba99cd5f8e
Raphael Antonietti <antonietti.raphael@gmail.com>
2019-08-23 15:59:49 +0200

    Added prices accordingly

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,8 @@ class Booking < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
   validates :status, inclusion: { in: %w(Pending Declined Accepted) }
+
+  def compute_price
+    self.price = (((end_date - start_date).to_i + 1) * kitchen.price || 0) + 8
+  end
 end
